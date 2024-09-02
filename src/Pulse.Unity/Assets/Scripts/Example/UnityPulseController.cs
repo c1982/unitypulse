b7d989c0436cee4f249695924aea57422fda5c94
c1982 <aspsrc@gmail.com>
2024-09-03 00:32:40 +0300

    chore: logs for UnityPulseController

--- src/Pulse.Unity/Assets/Scripts/Example/UnityPulseController.cs
+++ src/Pulse.Unity/Assets/Scripts/Example/UnityPulseController.cs
@@ -14,14 +14,15 @@ namespace Example
             
             _serializationTimeKeyName = System.Text.Encoding.UTF8.GetBytes("serialization_time");
             
-            _pulse = new UnityPulse("127.0.0.1", 7771);
+            _pulse = new UnityPulse("127.0.0.1", 7771, Application.targetFrameRate);
             _pulse.Start();
+            Debug.Log("UnityPulse started");
         }
         
         public void FixedUpdate()
         {
             _pulse.Collect();
-            _pulse.Collect(_serializationTimeKeyName, 1024);
+            _pulse.Collect(_serializationTimeKeyName, 5000);
         }
 
         public void OnDestroy()
