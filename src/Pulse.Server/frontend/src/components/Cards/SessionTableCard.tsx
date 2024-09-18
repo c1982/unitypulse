a9e962c5c261e08e4d1d3d11faf1042c74509e1d
Batu Yunculer <batuyunculer@gmail.com>
2024-09-19 02:35:40 +0300

    fix(SessionTableCard.tsx): import SessionDetail type from session types to fix type error feat(SessionTableCard.tsx): add functionality to fetch and display session detail data when "Details" button is clicked

--- src/Pulse.Server/frontend/src/components/Cards/SessionTableCard.tsx
+++ src/Pulse.Server/frontend/src/components/Cards/SessionTableCard.tsx
@@ -1,11 +1,19 @@
-import { SessionData } from '../../types/session';
+import { SessionData, SessionDetail } from '../../types/session';
 import { useUTCDate } from '../../hooks/useUTCDate';
 import { useState } from 'react';
+import { getSessionDetailByID } from '../../services/sessionService';
+import { SimpleLineChart } from '../Charts/SimpleLineChart';
 
 export const SessionTableCard: React.FC<{ session: SessionData }> = ({ session }) => {
     const startUTC = useUTCDate(session.start_time);
     const stopUTC = useUTCDate(session.stop_time);
     const [sessionDetailVisible, setSessionDetailVisible] = useState(false);
+    const [sessionDetail, setSessionDetail] = useState<SessionDetail | null>(null);
+
+    const fetchSessionDetail = async (sessionID: string) => {
+        const sessionDetailData = await getSessionDetailByID(sessionID);
+        setSessionDetail(sessionDetailData);
+    };
 
     return (
         <>
@@ -52,17 +60,25 @@ export const SessionTableCard: React.FC<{ session: SessionData }> = ({ session }
                     <button
                         className='rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
                         type='button'
-                        onClick={() => setSessionDetailVisible(!sessionDetailVisible)}>
+                        onClick={() => {
+                            fetchSessionDetail(session.session);
+                            setSessionDetailVisible(!sessionDetailVisible);
+                        }}>
                         Details
                     </button>
                 </td>
             </tr>
             {sessionDetailVisible && (
-                <tr
-                    className={
-                        sessionDetailVisible ? 'border-b border-dashed last:border-b-0' : ''
-                    }>
-                    <td>{sessionDetailVisible ? 'true' : 'false'}</td>
+                <tr>
+                    <td
+                        colSpan={7}
+                        className={
+                            sessionDetailVisible
+                                ? 'border-b border-dashed last:border-b-0 flex-1'
+                                : ''
+                        }>
+                        {sessionDetail ? <SimpleLineChart data={sessionDetail} /> : 'false'}
+                    </td>
                 </tr>
             )}
         </>
