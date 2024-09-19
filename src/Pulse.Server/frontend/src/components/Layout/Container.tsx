3c3ad89487d1ad5e6e5eb7eb06216b9e579386e2
Batu Yunculer <batuyunculer@gmail.com>
2024-09-20 01:19:50 +0300

    fix(Layout): add fixed positioning to Container component to prevent scrolling issues fix(Layout): add overflow-scroll to Main component to enable scrolling within the component

--- src/Pulse.Server/frontend/src/components/Layout/Container.tsx
+++ src/Pulse.Server/frontend/src/components/Layout/Container.tsx
@@ -3,7 +3,7 @@ import { Main } from './Main';
 
 export const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className='flex w-full flex'>
+        <div className='flex w-full flex fixed'>
             <Sidebar />
             <Main>{children}</Main>
         </div>
--- src/Pulse.Server/frontend/src/components/Layout/Main.tsx
+++ src/Pulse.Server/frontend/src/components/Layout/Main.tsx
@@ -4,7 +4,7 @@ export const Main: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div className='flex flex-col flex-1'>
             <Header />
-            <main className='flex flex-col w-full p-6'>{children}</main>
+            <main className='flex flex-col w-full p-6 h-screen overflow-scroll'>{children}</main>
         </div>
     );
 };
